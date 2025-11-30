fc6e915109d7cac44274bbee632bc516178c1df8
pizzariamassanostra <pizzariamassanostra@gmail.com>
2025-11-30 20:21:26 -0300

    fix: remove next export and fix config for Render

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', // 🔑 necessário para gerar a pasta "out"
 };
 
 export default nextConfig;
