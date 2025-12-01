cc769a3cde3f3a47df57d5d988420a021e23ee11
pizzariamassanostra <pizzariamassanostra@gmail.com>
2025-11-30 22:45:04 -0300

    Usando DATABASE_URL com pooling e IPv4

--- api/src/config/typeorm.config.ts
+++ api/src/config/typeorm.config.ts
@@ -1,29 +1,17 @@
-// ============================================
-// DATABASE CONFIG: POSTGRESQL + TYPEORM
-// ============================================
-// Configura e exporta a conexão com o banco de dados PostgreSQL
-// utilizando variáveis de ambiente do arquivo .env.
-// ============================================
-
 import { DataSource } from 'typeorm';
 import { config } from 'dotenv';
 
-// Carrega variáveis de ambiente do arquivo .env
 config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   entities: ['src/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
   synchronize: false,
   logging: true,
-  ssl: true, // garante conexão segura com Supabase
+  ssl: true,
   extra: {
-    family: 4, // força uso de IPv4
+    family: 4,
   },
 });
